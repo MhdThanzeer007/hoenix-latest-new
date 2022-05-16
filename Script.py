587b3d70678ba9839d1dfade53af43f4f3a5b1d0
MHD_THANZEER <89308356+MhdThanzeer007@users.noreply.github.com>
2022-05-16 15:10:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,96 +1,83 @@
 class script(object):
-    START_TXT = """𝐇𝐞𝐲 {}\n\n⎆𝐌𝐲 𝐍𝐚𝐦𝐞 𝐢𝐬 𝐏𝐡𝐨𝐞𝐧𝐢𝐱\n\n⎆𝐈 𝐀𝐦 𝐚 𝐀𝐝𝐯𝐚𝐧𝐜𝐞𝐝 𝐟𝐢𝐥𝐭𝐞𝐫 𝐁𝐨𝐭.\n\n⎆𝐖𝐨𝐫𝐤𝐢𝐧𝐠 𝐖𝐢𝐭𝐡 <a href='https://t.me/Cinemahub182'>𝐂𝐈𝐍𝐄𝐌𝐀𝐇𝐔𝐁...</a>\n\n➪ 𝐌𝐚𝐢𝐧𝐭𝐚𝐢𝐧𝐞𝐝 𝐁𝐲 : <a href='https://t.me/Kaithiofdarkness369'> 𝑆𝑜𝑜𝑟𝑦𝑎 𝐷𝑢𝑡ℎ </a>\n➪ 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝  𝐁𝐲 : <a href='https://t.me/Anandhukuttu'>  𝐴𝑛𝑎𝑛𝑑ℎ𝑢 </a>"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """<b>🥱 My Name : 𝐏𝐡𝐨𝐞𝐧𝐢𝐱
-🕵‍♂ Developer : <a href='https://t.me/Anandhukuttu'>★ 𝓐𝓷𝓪𝓷𝓭𝓱𝓾 ★</a>
-📚 Library : 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-🖥 Language : 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-🎪 Data Base : 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-🔋 Bot Group : @Cinemahub182 </b>"""
+    START_TXT = """<b>𝘏𝘦𝘭𝘭𝘰𝘸 {} \n𝘔𝘺 𝘕𝘢𝘮𝘦 𝘐𝘴 <a href=https://t.me/{}>{}</a> </b>
+
+<b>🌚 𝘏𝘦𝘺 𝘐 𝘈𝘮 𝘕𝘰𝘸 𝘐𝘯 𝘗𝘶𝘣𝘭𝘪𝘤 𝘠𝘰𝘶 𝘊𝘢𝘯 𝘜𝘴𝘦 𝘔𝘦 𝘛𝘰 𝘠𝘰𝘶𝘳 𝘎𝘳𝘰𝘶𝘱 𝘛𝘩𝘢𝘵'𝘴 𝘈𝘭𝘭 𝘔𝘢𝘯 𝘐 𝘞𝘪𝘭𝘭 𝘗𝘳𝘰𝘷𝘪𝘥𝘦 𝘔𝘰𝘷𝘪𝘦𝘴 𝘐𝘯 𝘗𝘔.
+
+✨ 𝐌𝐚𝐢𝐧𝐭𝐚𝐢𝐧𝐞𝐝 𝐁𝐲 :  <a href='https://t.me/mhd_thanzeer'>𝙈𝙃𝘿 𝙏𝙃𝘼𝙉𝙕𝙀𝙀𝙍</a> </b>"""
+    HELP_TXT = """ᕼEY {} ℍ𝕖𝕣𝕖 𝕋𝕙𝕖 ℍ𝕖𝕝𝕡 𝔽𝕠𝕣 𝕄𝕪 ℂ𝕠𝕞𝕞𝕒𝕟𝕕𝕤 ✨️"""
+    ABOUT_TXT = """<b>👨‍🔬 My Name : {}\n\n🕵‍♂ Developer : <a href='https://t.me/mhd_thanzeer'>𝙈𝙃𝘿 𝙏𝙃𝘼𝙉𝙕𝙀𝙀𝙍</a>\n\n📚 Library : 𝙿𝚁𝙾𝙶𝚁𝙰𝙼\n\n🖥 Language : 𝙿𝚈𝚃𝙷𝙾𝙽  ᴀɴᴅ  𝙲\n\n🎪 Data Base : 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱\n\n🔋 Bot Group : @wolfpackmedia </b>"""
     SOURCE_TXT = """<b>NOTE:</b>
-<b>𝖳𝗁𝗂𝗌 𝖡𝗈𝗍 𝖶𝖺𝗌 𝖬𝖺𝗄𝖾𝖽 𝖳𝖺𝗄𝗂𝗇𝗀 𝖲𝗈𝗆𝖺𝗇𝗒 𝖱𝖾𝗉𝗈𝗌 𝖮𝖿 𝖮𝗍𝗁𝖾𝗋 𝖪𝗂𝗇𝖽 𝖡𝗈𝗍𝗌
-𝖲𝖮𝖴𝖱𝖢𝖤 𝖢𝖮𝖣𝖤 ~ 𝖭𝖮𝖳 𝖠𝖵𝖠𝖨𝖫𝖠𝖡𝖫𝖤 𝖱𝖨𝖦𝖧𝖳 𝖭𝖮𝖶
-𝖮𝖳𝖧𝖤𝖱 𝖪𝖨𝖭𝖣 𝖡𝖮𝖳𝖲:
-𝖠𝖴𝖳𝖮 𝖥𝖨𝖫𝖳𝖤𝖱 : <a href=https://github.com/EvamariaTG/EvaMaria>𝖤𝗏𝖺 𝖬𝖺𝗋𝗂𝖺</a>
-𝖲𝖮𝖭𝖦 :  <a href=https://github.com/AsmSafone/RadioPlayerV2>𝖠𝗌𝗆𝖲𝖺𝖿𝗈𝗇𝖾</a>
-𝖥𝖨𝖫𝖳𝖤𝖱 : <a href=https://github.com/TroJanzHEX/Unlimited-Filter-Bot>𝖴𝗇𝗅𝗂𝗆𝗂𝗍𝖾𝖽 𝖥𝗂𝗅𝗍𝖾𝗋 𝖡𝗈𝗍</a></b>
-"""
-    SONG_TXT ="""<b>SONG MODULE</b>
-WE WILL ADD SOON"""
-    
-    YTVIDB_TXT = """<b>𝖣𝗈𝗐𝗇𝗅𝗈𝖺𝖽 𝖠𝗇𝗒 𝖬𝗉4 𝖸𝗈𝗎𝗍𝗎𝖻𝖾 𝖵𝗂𝖽𝖾𝗈𝗌..❣️</b>
-WE WILL ADD SOON</b>"""
-    
-    SUPPORT_TXT = """<b>𝖧𝖾𝗋𝖾 𝗂𝗌 𝖳𝗁𝖾 𝖮𝗎𝗋 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖦𝗋𝗈𝗎𝗉𝗌 𝖫𝗂𝗇𝗄𝗌...🙃
-𝖠𝗋𝖾 𝖸𝗈𝗎 𝖫𝗂𝗄𝖾 𝖳𝗁𝖾 𝖡𝗈𝗍 𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖮𝗎𝗋 𝖳𝖾𝖺𝗆 ☺️</b>"""
-    
-    TTS_TXT = """<b>🗣 TEXT To Speech
-A Module To Convert TEXT To Voice With Language Support
-📚 Avaible Command:
-◉ /tts :- Reply To Any TEXT Message With Language Code To Convert As Audio
-⚠️ Example :- /tts en (English)</b>"""
-    
-    MANUALFILTER_TXT = """Help: <b>Filters</b>
-- Filter is the feature were users can set automated replies for a particular keyword and tessa will respond whenever a keyword is found the message
+
+👋<b><i>എന്താടാ മോനെ നോക്കുന്നേ നിനക്ക് ആവശ്യമായിട്ടുള്ളത് ഇവിടെ ഇല്ല 😌
+
+</i></b>"""
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
+
+- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+
 <b>NOTE:</b>
-1. Bella Bot should have admin privillage.
+1. eva maria should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
+
 <b>Commands and Usage:</b>
 • /filter - <code>add a filter in chat</code>
 • /filters - <code>list all the filters of a chat</code>
 • /del - <code>delete a specific filter in chat</code>
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
-- Bella Bot Supports both url and alert inline buttons.
+
+- Eva Maria Supports both url and alert inline buttons.
+
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Bella Bot supports buttons with any telegram media type.
+2. Eva Maria supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
+
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https//t.me/Anandhukuttu)</code>
+<code>[Button Text](buttonurl:https://t.me/mhd_thanzeer)</code>
+
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
-    
-    AUTO_MANUAL_TXT = """Help: <b>Auto Filter</b>
+    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+
 <b>NOTE:</b>
 1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains cam rip, porn and fake files.
+2. make sure that your channel does not contains camrips, porn and fake files.
 3. Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
-    
-    AUTOFILTER_TXT= """Help: <b>Auto Filter</b>
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains cam rip, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
-    
     CONNECTION_TXT = """Help: <b>Connections</b>
+
 - Used to connect bot to PM for managing filters 
 - it helps to avoid spamming in groups.
+
 <b>NOTE:</b>
 1. Only admins can add a connection.
 2. Send <code>/connect</code> for connecting me to ur PM
+
 <b>Commands and Usage:</b>
 • /connect  - <code>connect a particular chat to your PM</code>
 • /disconnect  - <code>disconnect from a chat</code>
 • /connections - <code>list all your connections</code>"""
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
+
 <b>NOTE:</b>
-these are the extra features of tessa
+these are the extra features of Eva Maria
+
 <b>Commands and Usage:</b>
-• /id - <code>get id of a specifed user.</code>
+• /id - <code>get id of a specified user.</code>
 • /info  - <code>get information about a user.</code>
 • /imdb  - <code>get the film information from IMDb source.</code>
 • /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_NOTE = """Help: <b>Admin mods</b>
+    ADMIN_TXT = """Help: <b>Admin mods</b>
+
 <b>NOTE:</b>
 This module only works for my admins
+
 <b>Commands and Usage:</b>
 • /logs - <code>to get the rescent errors</code>
 • /stats - <code>to get status of files in db.</code>
+• /delete - <code>to delete a specific file from db.</code>
 • /users - <code>to get list of my users and ids.</code>
 • /chats - <code>to get list of the my chats and ids </code>
 • /leave  - <code>to leave from a chat.</code>
@@ -99,19 +86,15 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
-    TGRAPH_TXT = """Help: <b>TGraph & Paste</b>
-Do as you wish with telegra.ph module!
-<b>Commands and Usage:</b>
-• /tgmedia or /tgraph - upload supported media (within 5MB) to telegraph.
-<b>NOTE:</b>
-• IMDb should have admin privillage.
-• These commands works on both pm and group.
-• These commands can be used by any group member."""
+    STATUS_TXT = """<b>★ 𝘛𝘰𝘵𝘢𝘭 𝘍𝘪𝘭𝘦𝘴 :</b> <code>{}</code>
+
+<b>★ 𝘛𝘰𝘵𝘢𝘭 𝘜𝘴𝘦𝘳𝘴 :</b> <code>{}</code>
+
+<b>★ 𝘛𝘰𝘵𝘢𝘭 𝘊𝘩𝘢𝘵𝘴 :</b> <code>{}</code>
+
+<b>★ 𝘜𝘴𝘦𝘥 𝘚𝘵𝘰𝘳𝘢𝘨𝘦 :</b> <code>{}</code>
+
+<b>★ 𝘍𝘳𝘦𝘦 𝘚𝘵𝘰𝘳𝘢𝘨𝘦 :</b> <code>{}</code> """
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
